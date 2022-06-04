aae36bc99565e784efa5e26344858ea72e21bf27
Kotlin AB <kotlin@gmx.es>
2022-06-04 20:50:23 +0200

    Commit Funcionando

--- app/src/main/java/com/example/listacompra/MainActivity.kt
+++ app/src/main/java/com/example/listacompra/MainActivity.kt
@@ -1,15 +1,41 @@
 package com.example.listacompra
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
 import com.example.listacompra.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var b:ActivityMainBinding
+    private var lista = mutableMapOf<String, String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
+        lista.put("art1","2.3")
+    }
+    fun addItem (view:View){
+        if (b.etArticulo.text.isNotEmpty() and b.etArticulo.text.isNotEmpty()){
+            lista.put(b.etArticulo.text.toString(), b.etPrecio.text.toString())
+            b.etArticulo.text.clear()
+            b.etPrecio.text.clear()
+            msj(lista.toString())
+        } else {
+            msj("Todos los campos son obligatorios")
+        }
+    }
 
+    fun nextActivity(view:View){
+        val myIntent = Intent(this@MainActivity, ResultActivity::class.java)
+        lista.forEach { k,v ->
+            myIntent.putExtra(k,v)
+        }
+        startActivity(myIntent)
+    }
+    fun msj (str:String){
+        Toast.makeText(this, str, Toast.LENGTH_LONG).show()
     }
 }
---
+++ app/src/main/java/com/example/listacompra/ResultActivity.kt
@@ -0,0 +1,33 @@
+package com.example.listacompra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.listacompra.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+    private lateinit var b:ActivityResultBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        b = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(b.root)
+        b.tvLista.text = "LISTA\n\n"
+        val myExtras = intent.extras
+        if (myExtras != null){
+            val keys = myExtras.keySet()
+            for (key in keys){
+                b.tvLista.text = b.tvLista.text.toString()
+                    .plus("$key:\t\t\t${myExtras.get(key)}\n")
+                println("key: $key\tvalue: ${myExtras.get(key)}")
+            }
+        }
+        b.btnVolver.setOnClickListener {
+
+
+        }
+
+    }
+    fun msj (str:String){
+        Toast.makeText(this, str, Toast.LENGTH_LONG).show()
+    }
+}
